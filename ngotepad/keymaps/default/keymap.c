78c780c0c99aa8746e351a3fddcae66398252aa0
jango99 <ja.ngo7199@gmail.com>
2020-03-01 20:00:24 -0800

    changed binds

--- ngotepad/keymaps/default/keymap.c
+++ ngotepad/keymaps/default/keymap.c
@@ -39,19 +39,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
-void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
+void encoder_update_user(uint8_t index, bool clockwise) 
+{
+    if (index == 0) 
+	{
+        if (clockwise) 
+		{    
+			tap_code(KC_WH_D);
+        } 
+		else 
+		{
+            tap_code(KC_WH_U);
         }
     }
-    else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
+	else if (index == 1)
+	{
+		if (clockwise) 
+		{    
+			tap_code(KC_WH_L);
+        } 
+		else 
+		{
+            tap_code(KC_WH_R);
         }
-    }
+	}
 }
